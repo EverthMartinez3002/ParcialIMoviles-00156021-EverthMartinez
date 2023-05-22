81897c9d5a86f0ec643afe6dc597d03746c16a8a
EverthMartinez3002 <00156021@uca.edu.sv>
2023-05-22 13:46:52 -0600

    Cape Model

---
+++ ParcialI/app/src/main/java/com/example/parcialimoviles_00156021_everthmartinez/data/dummydata.kt
@@ -0,0 +1,14 @@
+package com.example.parcialimoviles_00156021_everthmartinez.data
+
+//Food one data
+val name = "Pupusas"
+val type = "Tipica"
+val description = "Es una comida muy rica"
+val qualification = "9.8"
+
+//Food two data
+val name2 = "Pupusas"
+val type2 = "Tipica"
+val description2 = "Es una comida muy rica"
+val qualification2 = "9.8"
+
---
+++ ParcialI/app/src/main/java/com/example/parcialimoviles_00156021_everthmartinez/data/model/FoodModel.kt
@@ -0,0 +1,8 @@
+package com.example.parcialimoviles_00156021_everthmartinez.data.model
+
+data class FoodModel (
+    val name: String,
+    val type: String,
+    val description: String,
+    val qualification: String,
+    )
