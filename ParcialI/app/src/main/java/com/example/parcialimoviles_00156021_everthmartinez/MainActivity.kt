769fee45fb3db60e3025895a2d42251cf25163ea
EverthMartinez3002 <00156021@uca.edu.sv>
2023-05-22 13:38:02 -0600

    fisrt commit initialize project

---
+++ ParcialI/app/src/main/java/com/example/parcialimoviles_00156021_everthmartinez/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.parcialimoviles_00156021_everthmartinez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
